b8ad9895b4d157ff52066e69430152f19bd0e41d
scikit-maad <43136563+scikit-maad@users.noreply.github.com>
2018-09-12 17:43:53 +0200

    Add files via upload

--- maad/sound/__init__.py
+++ maad/sound/__init__.py
@@ -10,11 +10,9 @@
 from ._sound_func import (load,
                          select_bandwidth,
                          spectrogram,
-                         convert_dt_df_into_points,
                          preprocess_wrapper)
 
 __all__ = ['load', 
            'select_bandwidth',
            'spectrogram',
-           'convert_dt_df_into_points',
            'preprocess_wrapper']
