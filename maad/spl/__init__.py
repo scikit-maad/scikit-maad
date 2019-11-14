c968cb6d7f230e9f99f5bdde62f6500f27cd2e42
Sylvain HAUPERT <sylvain.haupert@mnhn.fr>
2019-11-14 17:43:48 +0100

    Minor changes
    In sound_func.py : db_range=120 in order to restrict the conversion into dB values
    
    In init.py of SPL, change from.spl to from.wavedBSPL

--- maad/spl/__init__.py
+++ maad/spl/__init__.py
@@ -1,19 +1,20 @@
 # -*- coding: utf-8 -*-
-""" Utility functions for scikit-maad
-Collection of miscelaneous functions that help to simplify the framework
+""" SPL conversion functions for scikit-maad
+Collection of miscelaneous functions that help to convert wav and volt date
+to Sound Pressure Level (SPL in Pascal) and Leq (Continuous Equivalent SPL)
 
 """
 
-from .spl import (wav2volt,
-                  volt2SPL,
-                  wav2SPL,
-                  SPL2dBSPL,
-                  wav2dBSPL,
-                  wav2Leq,
-                  wavSPL2Leq,
-                  energy2dBSPL,
-                  dBSPL2energy,
-                  PSD2Leq)
+from .wavedBSPL import (wav2volt,
+                        volt2SPL,
+                        wav2SPL,
+                        SPL2dBSPL,
+                        wav2dBSPL,
+                        wav2Leq,
+                        wavSPL2Leq,
+                        energy2dBSPL,
+                        dBSPL2energy,
+                        PSD2Leq)
 
 __all__ = ['wav2volt',
            'volt2SPL',
