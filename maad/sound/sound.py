ac528221fddde7ed392d698ee445876321c7d243
Juan Sebastian Ulloa <jseb.ulloa@gmail.com>
2020-11-01 21:09:34 -0500

    minor change. uncomment a line during testing

--- maad/sound/sound.py
+++ maad/sound/sound.py
@@ -17,7 +17,7 @@ import numpy as np
 import scipy as sp 
 from scipy.io import wavfile 
 from scipy.signal import hilbert, sosfiltfilt, convolve, iirfilter, get_window
-#from maad.util import plot1D, plot2D, crop_image, power2dBSPL, pressure2dBSPL 
+from maad.util import plot1D, plot2D, crop_image, power2dBSPL, pressure2dBSPL 
 
 # =============================================================================
 # private functions
