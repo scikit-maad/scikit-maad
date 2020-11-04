70915e96c6eee56250395c40253c7c18e79b9025
Juan Sebastian Ulloa <jseb.ulloa@gmail.com>
2020-11-03 20:57:15 -0500

    Update name math_tools.py to avoid conflicts with numpy

--- maad/util/__init__.py
+++ maad/util/__init__.py
@@ -21,7 +21,7 @@ from .visualization import (rand_cmap,
                    plot1D,
                    plot2D)
 
-from .math import (running_mean,
+from .math_tools import (running_mean,
                		get_unimode)
 
 from .parser import (read_audacity_annot,
