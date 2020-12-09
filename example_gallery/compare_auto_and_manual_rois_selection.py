bbd08992b0a964d41396ee5b2008cbf7504702ba
Sylvain Haupert <sylvain.haupert@mnhn.fr>
2020-12-09 11:33:45 +0100

    Add new example
    Replace find_rois

--- example_gallery/compare_auto_and_manual_rois_selection.py
+++ example_gallery/compare_auto_and_manual_rois_selection.py
@@ -13,11 +13,12 @@ by different image processing tools such as binarization, double thresholding,
 mathematical morphology tools...
 
 Dependencies: To execute this example you will need to have installed the 
-scikit-image and pandas Python packages.
+scikit-image, scikit-learn and pandas Python packages.
 
 """
-# sphinx_gallery_thumbnail_path = '../_images/sphx_glr_find_rois.png'
+# sphinx_gallery_thumbnail_path = '../_images/sphx_glr_compare_auto_and_manual_rois_selection.png'
 
+import numpy as np
 from maad import sound, rois, features
 from maad.util import power2dB, plot2D, format_features, read_audacity_annot
 
@@ -51,7 +52,7 @@ plot2D(Sxx_db, **{'vmin':0, 'vmax':dB_max, 'extent':ext})
 # aggregated, meaning that there is no more connected pixels with value upper 
 # than the second threshold value.
 
-# First we remove the stationary background in order to increase the contrast
+# First we remove the stationary background in order to increase the contrast [1]
 Sxx_db_noNoise, _ = rois.remove_background_along_axis(Sxx_db, mode='ale', 
                                                  display=True, 
                                                  ext=ext)
@@ -87,25 +88,67 @@ ax, fig = features.overlay_centroid(Sxx_db, ext, df_centroid, savefig=None,
                                     fig=fig, ax=ax)
 
 #%% 
-# Now, we can compare with manual annotation performed with Audacity software.
+# Let's compare with the manual annotation (Ground Truth GT) obtained with 
+# Audacity software.
 # Each acoustic signature is manually selected and labeled. All similar acoustic 
 # signatures are labeled with the same name
-df_rois = read_audacity_annot('../data/cold_forest_daylight_label.txt')  ## annotations using Audacity
+df_rois_GT = read_audacity_annot('../data/cold_forest_daylight_label.txt')  ## annotations using Audacity
+
+# drop rows with frequency and time outside of tn and fn
+df_rois_GT = df_rois_GT[(df_rois_GT.min_t >= tn.min()) & 
+                        (df_rois_GT.max_t <= tn.max()) & 
+                        (df_rois_GT.min_f >= fn.min()) & 
+                        (df_rois_GT.max_f <= fn.max())]
 
 # format dataframe df_rois in order to convert time and frequency into pixels
-df_rois = format_features(df_rois, tn, fn)
+df_rois_GT = format_features(df_rois_GT, tn, fn)
 
 # overlay bounding box on the original spectrogram
-ax, fig = rois.overlay_rois(Sxx_db, ext, df_rois, vmin=0, vmax=96)
+ax, fig = rois.overlay_rois(Sxx_db, ext, df_rois_GT, vmin=0, vmax=96)
     
 # Compute and visualize centroids
-df_centroid = features.centroid_features(Sxx_db, df_rois)
-df_centroid = format_features(df_centroid, tn, fn)
-ax, fig = features.overlay_centroid(Sxx, ext, df_centroid, savefig=None, 
+df_centroid_GT = features.centroid_features(Sxx_db, df_rois_GT)
+df_centroid_GT = format_features(df_centroid_GT, tn, fn)
+ax, fig = features.overlay_centroid(Sxx, ext, df_centroid_GT, savefig=None, 
                                     vmin=-0, vmax=96, ms=2, color='blue',
+                                    fig=fig, ax=ax)
+
+# print informations about the rois
+print ('Total number of ROIs : %2.0f' %len(df_rois_GT))
+print ('Number of different ROIs : %2.0f' %len(np.unique(df_rois_GT['label'])))
+
+#%%
+# Now we cluster the ROIS depending on 3 ROIS features :
+# - centroid_f : frequency position of the roi centroid 
+# - duration_t : duration of the roi
+# - bandwidth_f : frequency bandwidth of the roi
+# The clustering is done by the so-called KMeans clustering algorithm.
+# The number of attended clustering is the number of clusters found with 
+# manual annotation.
+# Finally, each rois is labeled with the corresponding cluster number predicted
+# by KMeans
+from sklearn.cluster import KMeans
+
+# select features to perform KMeans clustering
+FEATURES = [ 'centroid_f','duration_t','bandwidth_f']
+
+# perform KMeans with the same number of clusters as with the manual annotation  
+NN_CLUSTERS = len(np.unique(df_rois_GT['label'])) 
+labels = KMeans(n_clusters=NN_CLUSTERS, random_state=0).fit_predict(df_centroid[FEATURES])
+
+# Replace the unknow label by the cluster number predicted by KMeans
+df_centroid['label'] = [str(i) for i in labels] 
+
+# overlay color bounding box corresponding to the label, and centroids
+# on the original spectrogram
+ax, fig = rois.overlay_rois(Sxx_db, ext, df_centroid, vmin=0, vmax=96)
+ax, fig = features.overlay_centroid(Sxx, ext, df_centroid, savefig=None, 
+                                    vmin=-0, vmax=96, ms=2,
                                     fig=fig, ax=ax)
 
 #%% 
 # References
 # -----------
-# 1. Sifre, L., & Mallat, S. (2013). Rotation, scaling and deformation invariant scattering for texture discrimination. Computer Vision and Pattern Recognition (CVPR), 2013 IEEE Conference On, 1233–1240. http://ieeexplore.ieee.org/xpls/abs_all.jsp?arnumber=6619007
+# 1.Towsey, M., 2013b. Noise Removal from Wave-forms and Spectrograms Derived from
+#   Natural Recordings of the Environment. Queensland University of Technology,
+#   Brisbane
