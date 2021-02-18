51bc158333df92a225803f882bfa5615c145c86c
Sylvain Haupert <sylvain.haupert@mnhn.fr>
2021-02-18 22:55:15 +0100

    Minor change : format_features
    Each call of format_features will update pixels unit according to time and frequency vectors given as input argument

--- maad/util/miscellaneous.py
+++ maad/util/miscellaneous.py
@@ -839,99 +839,141 @@ def format_features(df, tn, fn):
     if type(df) is not pd.core.frame.DataFrame:
         raise TypeError('Rois must be of type pandas DataFrame')
 
-    if ('min_t' and 'min_f' and 'max_t' and 'max_f') in df and not (('min_y' and 'min_x' and 'max_y' and 'max_x') in df):
+    # Do the loop twice in order to force updating coordinates and parameters
+    # in pixels unit (time and frequency do not change when the time and 
+    # frequency vectors resolution change)
+    count = 0
+    while (count < 2):
 
-        # Check that time and frequency limits are inside tn and fn limits
-        # df = df[(df.min_t >= tn.min()) & (df.max_t <= tn.max()) & (df.min_f >= fn.min()) & (df.max_f <= fn.max())]
-        if (df.min_t < tn.min()).any() | (df.max_t > tn.max()).any() | (df.min_f < fn.min()).any() | (df.max_f > fn.max()).any():
-            warnings.warn(
-                'ROIs boundaries are outside time or frecuency signal limits. Clipping ROIS to valid boundaries.')
-            df.loc[df.min_t < tn.min(), 'min_t'] = tn.min()
-            df.loc[df.max_t > tn.max(), 'max_t'] = tn.max()
-            df.loc[df.min_f < fn.min(), 'min_f'] = fn.min()
-            df.loc[df.max_f > fn.max(), 'max_f'] = fn.max()
+        # if ('min_t' and 'min_f' and 'max_t' and 'max_f') in df and not (('min_y' and 'min_x' and 'max_y' and 'max_x') in df):
+        if ('min_t' and 'min_f' and 'max_t' and 'max_f') in df:
+            # Check that time and frequency limits are inside tn and fn limits
+            # df = df[(df.min_t >= tn.min()) & (df.max_t <= tn.max()) & (df.min_f >= fn.min()) & (df.max_f <= fn.max())]
+            if (df.min_t < tn.min()).any() | (df.max_t > tn.max()).any() | (df.min_f < fn.min()).any() | (df.max_f > fn.max()).any():
+                warnings.warn(
+                    'ROIs boundaries are outside time or frecuency signal limits. Clipping ROIS to valid boundaries.')
+                df.loc[df.min_t < tn.min(), 'min_t'] = tn.min()
+                df.loc[df.max_t > tn.max(), 'max_t'] = tn.max()
+                df.loc[df.min_f < fn.min(), 'min_f'] = fn.min()
+                df.loc[df.max_f > fn.max(), 'max_f'] = fn.max()
 
-        df_bbox = []
-        for idx in df.index:
-            min_y = nearest_idx(fn, df.loc[idx, 'min_f'])
-            min_x = nearest_idx(tn, df.loc[idx, 'min_t'])
-            max_y = nearest_idx(fn, df.loc[idx, 'max_f'])
-            max_x = nearest_idx(tn, df.loc[idx, 'max_t'])
-            # test if max > min, if not, drop row form df
-            if (min_y <= max_y) and (min_x <= max_x):
-                df_bbox.append((min_y, min_x, max_y, max_x))
+            df_bbox = []
+            for idx in df.index:
+                min_y = nearest_idx(fn, df.loc[idx, 'min_f'])
+                min_x = nearest_idx(tn, df.loc[idx, 'min_t'])
+                max_y = nearest_idx(fn, df.loc[idx, 'max_f'])
+                max_x = nearest_idx(tn, df.loc[idx, 'max_t'])
+                # test if max > min, if not, drop row form df
+                if (min_y <= max_y) and (min_x <= max_x):
+                    df_bbox.append((min_y, min_x, max_y, max_x))
+                else:
+                    df = df.drop(idx)
+
+            # check if new columns
+            if not (('min_y' and 'min_x' and 'max_y' and 'max_x') in df):
+                df = df.join(pd.DataFrame(df_bbox,
+                                          columns=['min_y', 'min_x',
+                                                   'max_y', 'max_x'],
+                                          index=df.index))
+            # otherwise update the existing columns with new values
             else:
-                df = df.drop(idx)
+                df.update(pd.DataFrame(df_bbox,
+                                       columns=['min_y', 'min_x',
+                                                'max_y', 'max_x']))
 
-        df = df.join(pd.DataFrame(df_bbox,
-                                  columns=['min_y', 'min_x', 'max_y', 'max_x'],
-                                  index=df.index))
+        # if ('min_y' and 'min_x' and 'max_y' and 'max_x') in df and not (('min_t' and 'min_f' and 'max_t' and 'max_f') in df):
+        if ('min_y' and 'min_x' and 'max_y' and 'max_x') in df:
+            df_bbox = []
+            for _, row in df.iterrows():
+                min_f = fn[int(round(row.min_y))]
+                min_t = tn[int(round(row.min_x))]
+                max_f = fn[int(round(row.max_y))]
+                max_t = tn[int(round(row.max_x))]
+                # test if max > min, if not, drop row form df
+                if (min_f <= max_f) and (min_t <= max_t):
+                    df_bbox.append((min_f, min_t, max_f, max_t))
+                else:
+                    df = df.drop(row.name)
 
-    if ('min_y' and 'min_x' and 'max_y' and 'max_x') in df and not (('min_t' and 'min_f' and 'max_t' and 'max_f') in df):
-        df_bbox = []
-        for _, row in df.iterrows():
-            min_f = fn[int(round(row.min_y))]
-            min_t = tn[int(round(row.min_x))]
-            max_f = fn[int(round(row.max_y))]
-            max_t = tn[int(round(row.max_x))]
-            # test if max > min, if not, drop row form df
-            if (min_f <= max_f) and (min_t <= max_t):
-                df_bbox.append((min_f, min_t, max_f, max_t))
+            # check if new columns
+            if not (('min_t' and 'min_f' and 'max_t' and 'max_f') in df):
+                df = df.join(pd.DataFrame(df_bbox,
+                                          columns=['min_f', 'min_t',
+                                                   'max_f', 'max_t'],
+                                          index=df.index))
+
+        # if ('centroid_y' and 'centroid_x') in df and not (('centroid_f' and 'centroid_t') in df):
+        if ('centroid_y' and 'centroid_x') in df:
+            df_centroid = []
+            for _, row in df.iterrows():
+                centroid_f = fn[int(round(row.centroid_y))]
+                centroid_t = tn[int(round(row.centroid_x))]
+                df_centroid.append((centroid_f, centroid_t))
+                
+            # check if new columns
+            if not (('centroid_f' and 'centroid_t') in df) :
+                df = df.join(pd.DataFrame(df_centroid,
+                                          columns=['centroid_f', 'centroid_t'],
+                                          index=df.index))
+
+        # if ('centroid_f' and 'centroid_t') in df and not (('centroid_y' and 'centroid_x') in df):
+        if ('centroid_f' and 'centroid_t') in df:
+            df_centroid = []
+            for idx in df.index:
+                centroid_y = nearest_idx(fn, df.loc[idx, 'centroid_f'])
+                centroid_x = nearest_idx(tn, df.loc[idx, 'centroid_t'])
+                df_centroid.append((centroid_y, centroid_x))
+
+            # check if new columns
+            if not (('centroid_y' and 'centroid_x') in df) :
+                df = df.join(pd.DataFrame(df_centroid,
+                                          columns=['centroid_y', 'centroid_x'],
+                                          index=df.index))
+            # otherwise update the existing columns with new values
             else:
-                df = df.drop(row.name)
+                df.update(pd.DataFrame(df_centroid,
+                                       columns=['centroid_y', 'centroid_x']))
+                
+        # =============
+        # if ('duration_x' and 'bandwidth_y' and 'area_xy') in df and not (('duration_t' and 'bandwidth_f' and 'area_tf') in df):
+        if ('duration_x' and 'bandwidth_y' and 'area_xy') in df:
+            df_area = []
+            for _, row in df.iterrows():
+                bandwidth_f = row.bandwidth_y * (fn[1]-fn[0])
+                duration_t = row.duration_x * (tn[1]-tn[0])
+                area_tf = row.area_xy * (fn[1]-fn[0]) * (tn[1]-tn[0])
+                df_area.append((duration_t, bandwidth_f, area_tf))
+                
+            # check if new columns
+            if not (('duration_t' and 'bandwidth_f' and 'area_tf') in df) :
+                df = df.join(pd.DataFrame(df_area,
+                                          columns=['duration_t',
+                                                    'bandwidth_f', 'area_tf'],
+                                          index=df.index))
+ 
+        # if ('duration_t' and 'bandwidth_f' and 'area_tf') in df and not (('duration_x' and 'bandwidth_y' and 'area_xy') in df):
+        if ('duration_t' and 'bandwidth_f' and 'area_tf') in df:
+            df_area = []
+            for idx in df.index:
+                bandwidth_y = round(
+                    df.loc[idx, 'bandwidth_f']) / ((fn[1]-fn[0]))
+                duration_x = round(df.loc[idx, 'duration_t']) / (tn[1]-tn[0])
+                area_xy = round(df.loc[idx, 'area_xy'] /
+                                ((fn[1]-fn[0]) * (tn[1]-tn[0])))
+                df_area.append((bandwidth_y, duration_x, area_xy))
 
-        df = df.join(pd.DataFrame(df_bbox,
-                                  columns=['min_f', 'min_t', 'max_f', 'max_t'],
-                                  index=df.index))
+            # check if new columns
+            if not (('duration_x' and 'bandwidth_y' and 'area_xy') in df):
+                df = df.join(pd.DataFrame(df_area,
+                                          columns=['duration_x',
+                                                    'bandwidth_y', 'area_xy'],
+                                          index=df.index))
+            # otherwise update the existing columns with new values
+            else:
+                df.update(pd.DataFrame(df_area,
+                                       columns=['duration_x',
+                                                'bandwidth_y', 'area_xy']))
 
-    if ('centroid_y' and 'centroid_x') in df and not (('centroid_f' and 'centroid_t') in df):
-        df_centroid = []
-        for _, row in df.iterrows():
-            centroid_f = fn[int(round(row.centroid_y))]
-            centroid_t = tn[int(round(row.centroid_x))]
-            df_centroid.append((centroid_f, centroid_t))
-
-        df = df.join(pd.DataFrame(df_centroid,
-                                  columns=['centroid_f', 'centroid_t'],
-                                  index=df.index))
-
-    if ('centroid_f' and 'centroid_t') in df and not (('centroid_y' and 'centroid_x') in df):
-        df_centroid = []
-        for idx in df.index:
-            centroid_y = nearest_idx(fn, df.loc[idx, 'centroid_f'])
-            centroid_x = nearest_idx(tn, df.loc[idx, 'centroid_t'])
-            df_centroid.append((centroid_y, centroid_x))
-
-        df = df.join(pd.DataFrame(df_centroid,
-                                  columns=['centroid_y', 'centroid_x'],
-                                  index=df.index))
-
-    # =============
-    if ('duration_x' and 'bandwidth_y' and 'area_xy') in df and not (('duration_t' and 'bandwidth_f' and 'area_tf') in df):
-        df_area = []
-        for _, row in df.iterrows():
-            bandwidth_f = row.bandwidth_y * (fn[1]-fn[0])
-            duration_t = row.duration_x * (tn[1]-tn[0])
-            area_tf = row.area_xy * (fn[1]-fn[0]) * (tn[1]-tn[0])
-            df_area.append((duration_t, bandwidth_f, area_tf))
-
-        df = df.join(pd.DataFrame(df_area,
-                                  columns=['duration_t',
-                                           'bandwidth_f', 'area_tf'],
-                                  index=df.index))
-
-    if ('duration_t' and 'bandwidth_f' and 'area_tf') in df and not (('duration_x' and 'bandwidth_y' and 'area_xy') in df):
-        df_area = []
-        for idx in df.index:
-            bandwidth_y = round(df.loc[idx, 'bandwidth_f']) / ((fn[1]-fn[0]))
-            duration_x = round(df.loc[idx, 'duration_t']) / (tn[1]-tn[0])
-            area_xy = round(df.loc[idx, 'area_xy'] /
-                            ((fn[1]-fn[0]) * (tn[1]-tn[0])))
-            df_area.append((bandwidth_y, duration_x, area_xy))
-
-        df = df.join(pd.DataFrame(df_area,
-                                  columns=['duration_x',
-                                           'bandwidth_y', 'area_xy'],
-                                  index=df.index))
+        count += 1
 
     return df
