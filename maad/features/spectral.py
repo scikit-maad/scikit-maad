a90e927ec8028fed17fe7f7c69ac96c6e6024e18
Sebastian Aguilera Novoa <42812846+saguileran@users.noreply.github.com>
2023-01-19 18:11:34 -0500

    Added new traits
    Traits added:
    * Spectral
       - min_frequency
       - peak_frequency
       - spectral_quantile
       - bandwidth
    *Temporal
       - temporal_quantile
       - temporal_duration
       - min_time
       - peak_time
    All traits have docstring and gave comparable results.

--- maad/features/spectral.py
+++ maad/features/spectral.py
@@ -74,7 +74,86 @@ def spectral_moments (X, axis=None):
     return moments(X,axis)
 
 #%%
-def peak_frequency(s, fs, nperseg=1024, roi=None, method='best'):
+def min_frequency(s, fs, method='best', nperseg=1024, roi=None, as_pandas_series=False, **kwargs):
+    """
+    Compute the minimum frequency for audio signal. The minimum frequency is the frequency of
+    minimum power. If a region of interest with time and spectral limits is provided,
+    the minimum frequency is computed on the selection.
+    
+    Parameters
+    ----------
+    s : 1D array
+        Input audio signal
+    fs : float
+        Sampling frequency of audio signal
+    method : {'fast', 'best'}, optional
+        Method used to compute the peak frequency. 
+        The default is 'best'.
+    nperseg : int, optional
+        Length of segment to compute the FFT. The default is 1024.
+    roi : pandas.Series, optional
+        Region of interest where peak frequency will be computed. 
+        Series must have a valid input format with index: min_t, min_f, max_t, max_f.
+        The default is None.
+    as_pandas_series: bool
+        Return data as a pandas.Series. This is usefull when computing multiple features
+        over a signal. Default is False.
+    kwargs : additional keyword arguments
+        If `window='hann'`, additional keyword arguments to pass to
+        `sound.spectrum`.
+
+    Returns
+    -------
+    min_freq : float
+        Minimum frequency of audio segment.
+    amp_min_freq : float
+        Amplitud of the minimum frequency of audio segment.
+
+    Examples
+    --------
+    >>> from maad import features, sound
+    >>> s, fs = sound.load('../../data/spinetail.wav')
+    
+    Compute peak frequency with the fast method 
+    
+    >>> min_freq, min_freq_amp = features.peak_frequency(s, fs)
+    >>> print('Minimum Frequency: {:.5f} / Minimum Frequency Amplitud: {:.5E}'.format(min_freq, min_freq_amp))
+    Minimum Frequency: 6634.16016 / Minimum Frequency Amplitud: 1.29992E-04
+
+    """
+    if roi is None:
+        pxx, fidx = sound.spectrum(s, fs, nperseg, **kwargs)
+    else:
+        pxx, fidx = sound.spectrum(s, fs, nperseg, 
+                                   tlims=[roi.min_t, roi.max_t], 
+                                   flims=[roi.min_f, roi.max_f], 
+                                   **kwargs)
+    pxx = pd.Series(pxx, index=fidx)
+        
+    # simplest form to get peak frequency, but less precise
+    if method=='fast':
+        min_freq = pxx.idxmin() 
+        amp_min_freq = pxx[min_freq]
+    
+    elif method=='best':        
+        # use interpolation get more precise peak frequency estimation
+        idxmin = pxx.index.get_loc(pxx.idxmin())
+        x = pxx.iloc[[idxmin-1, idxmin, idxmin+1]].index.values
+        y = pxx.iloc[[idxmin-1, idxmin, idxmin+1]].values
+        f = interpolate.interp1d(x,y, kind='quadratic')
+        xnew = np.arange(x[0], x[2], 1)
+        ynew = f(xnew) 
+        min_freq = xnew[ynew.argmin()]
+        amp_min_freq = ynew[ynew.argmin()]
+
+    else:
+        raise Exception("Invalid method. Method should be 'fast' or 'best' ")
+    
+    if as_pandas_series:    return pd.Series([amp_min_freq], index=[min_freq])
+    else:                   return np.array([min_freq, amp_min_freq])
+
+#%%
+def peak_frequency(s, fs, method='best', nperseg=1024, roi=None, as_pandas_series=False, **kwargs):
     """
     Compute the peak frequency for audio signal. The peak frequency is the frequency of
     maximum power. If a region of interest with time and spectral limits is provided,
@@ -86,33 +165,54 @@ def peak_frequency(s, fs, nperseg=1024, roi=None, method='best'):
         Input audio signal
     fs : float
         Sampling frequency of audio signal
+    method : {'fast', 'best'}, optional
+        Method used to compute the peak frequency. 
+        The default is 'best'.
     nperseg : int, optional
         Length of segment to compute the FFT. The default is 1024.
     roi : pandas.Series, optional
         Region of interest where peak frequency will be computed. 
         Series must have a valid input format with index: min_t, min_f, max_t, max_f.
         The default is None.
-    method : {'fast', 'best'}, optional
-        Method used to compute the peak frequency. 
-        The default is 'fast'.
+    as_pandas_series: bool
+        Return data as a pandas.Series. This is usefull when computing multiple features
+        over a signal. Default is False.
+    kwargs : additional keyword arguments
+        If `window='hann'`, additional keyword arguments to pass to
+        `sound.spectrum`.
 
     Returns
     -------
     peak_freq : float
         Peak frequency of audio segment.
+    amp_peak_freq : float
+        Amplitud of the peak frequency of audio segment.
+
+    Examples
+    --------
+    >>> from maad import features, sound
+    >>> s, fs = sound.load('../../data/spinetail.wav')
+    
+    Compute peak frequency with the fast method 
+    
+    >>> peak_freq, peak_freq_amp = features.peak_frequency(s, fs)
+    >>> print('Peak Frequency: {:.5f} / Peak Frequency Amplitud: {:.5f}'.format(peak_freq, peak_freq_amp))
+    Peak Frequency: 6634.16016 / Peak Frequency Amplitud: 0.00013
 
     """
     if roi is None:
-        pxx, fidx = sound.spectrum(s, fs, nperseg)
+        pxx, fidx = sound.spectrum(s, fs, nperseg, **kwargs)
     else:
         pxx, fidx = sound.spectrum(s, fs, nperseg, 
                                    tlims=[roi.min_t, roi.max_t], 
-                                   flims=[roi.min_f, roi.max_f])
+                                   flims=[roi.min_f, roi.max_f],
+                                   **kwargs)
     pxx = pd.Series(pxx, index=fidx)
         
     # simplest form to get peak frequency, but less precise
     if method=='fast':
         peak_freq = pxx.idxmax() 
+        amp_peak_freq = pxx[peak_freq]
     
     elif method=='best':        
         # use interpolation get more precise peak frequency estimation
@@ -123,16 +223,19 @@ def peak_frequency(s, fs, nperseg=1024, roi=None, method='best'):
         xnew = np.arange(x[0], x[2], 1)
         ynew = f(xnew) 
         peak_freq = xnew[ynew.argmax()]
+        amp_peak_freq = ynew[ynew.argmax()]
 
     else:
         raise Exception("Invalid method. Method should be 'fast' or 'best' ")
     
-    return peak_freq
+    if as_pandas_series:    return pd.Series([amp_peak_freq], index=[peak_freq])
+    else:                   return np.array([peak_freq, amp_peak_freq])
 
 #%%
-def spectral_quantile(s, fs, q, nperseg=1024, roi=None):
+def spectral_quantile(s, fs, q=[0.05, 0.25, 0.5, 0.75, 0.95], nperseg=1024, roi=None, as_pandas_series=False, **kwargs):
     """
-    Compute the q-th quantile of the power spectrum.
+    Compute the q-th quantile of the power spectrum. If a region of interest with time and spectral limits is provided,
+    the q-th quantile is computed on the selection.
 
     Parameters
     ----------
@@ -140,20 +243,37 @@ def spectral_quantile(s, fs, q, nperseg=1024, roi=None):
         Input audio signal
     fs : float
         Sampling frequency of audio signal
-    q : array or float
+    q : array or float, optional
         Quantile or sequence of quantiles to compute, which must be between 0 and 1
         inclusive.
+        The defaul is [0.05, 0.25, 0.5, 0.75, 0.95].
     nperseg : int, optional
         Length of segment to compute the FFT. The default is 1024.
     roi : pandas.Series, optional
         Region of interest where peak frequency will be computed. 
         Series must have a valid input format with index: min_t, min_f, max_t, max_f.
         The default is None.
-
+    as_pandas_series: bool
+        Return data as a pandas.Series. This is usefull when computing multiple features
+        over a signal. Default is False.
+    kwargs : additional keyword arguments
+        If `window='hann'`, additional keyword arguments to pass to
+        `sound.spectrum`.
     Returns
     -------
-    Pandas Series
+    Pandas Series or Numpy array
         Quantiles of power spectrum. 
+
+    Examples
+    --------
+    >>> from maad import sound, features
+    >>> s, fs = sound.load('../../data/spinetail.wav')
+    
+    Compute the q-th quantile of the power spectrum
+
+    >>> qs = features.spectral_quantile(s, fs, [0.05, 0.25, 0.5, 0.75, 0.95])
+    >>> print("5%: {:.2f} / 25%: {:.2f} / 50%: {:.2f} / 75%: {:.2f} / 95%: {:.2f}".format(qs[0], qs[1], qs[2], qs[3], qs[4]))
+    5%: 6029.30 / 25%: 6416.89 / 50%: 6632.23 / 75%: 6890.62 / 95%: 9216.21
 
     """
     q = np.asanyarray(q)
@@ -162,11 +282,11 @@ def spectral_quantile(s, fs, q, nperseg=1024, roi=None):
     
     # Compute spectrum
     if roi is None:
-        pxx, fidx = sound.spectrum(s, fs, nperseg)
+        pxx, fidx = sound.spectrum(s, fs, nperseg, **kwargs)
     else:
-        pxx, fidx = sound.spectrum(s, fs, nperseg, 
+        pxx, fidx = sound.spectrum(s, fs, nperseg,
                                    tlims=[roi.min_t, roi.max_t], 
-                                   flims=[roi.min_f, roi.max_f])
+                                   flims=[roi.min_f, roi.max_f], **kwargs)
     pxx = pd.Series(pxx, index=fidx)
 
     # Compute spectral q
@@ -174,5 +294,81 @@ def spectral_quantile(s, fs, q, nperseg=1024, roi=None):
     spec_quantile = list()
     for quantile in q:
         spec_quantile.append(pxx.index[np.where(norm_cumsum>=quantile)[0][0]])
+    
+    if as_pandas_series:  return pd.Series(spec_quantile, index=[qth for qth in q])
+    else:                 return np.array(spec_quantile)
+#%%
+def bandwidth(s, fs, nperseg=1024, roi=None,  bw_method="quantile", as_pandas_series=False, **kwargs):
+    """
+    Compute the bandwith of the power spectrum. If a region of interest with time and spectral limits is provided,
+    the bandwidth is computed on the selection.
 
-    return pd.Series(spec_quantile, index=q)
+    Parameters
+    ----------
+    s : 1D array
+        Input audio signal
+    fs : float
+        Sampling frequency of audio signal
+    nperseg : int, optional
+        Length of segment to compute the FFT. The default is 1024.
+    roi : pandas.Series, optional
+        Region of interest where peak frequency will be computed. 
+        Series must have a valid input format with index: min_t, min_f, max_t, max_f.
+        The default is None.
+    bw_method : {'quantile', '3dB'}, optional
+        Method used to compute the spectral bandwidth. 
+        The default is 'quantile'.
+    as_pandas_series: bool
+        Return data as a pandas.Series. This is usefull when computing multiple features
+        over a signal. Default is False.
+    kwargs : additional keyword arguments
+        If `window='hann'`, additional keyword arguments to pass to
+        `sound.spectrum`.
+    Returns
+    -------
+    bandwidth : float 
+        Bandwidth 50% of the audio
+    bandwidth_90 : float 
+        Bandwidth 90%  of the audio
+    
+    Examples
+    --------
+    >>> from maad import features, sound
+    >>> s, fs = sound.load('../../data/spinetail.wav')
+
+    Compute the bandwidth of the power spectrum
+
+    >>> bw, bw_90 = features.bandwidth(s, fs)
+    >>> print("Bandwidth: {:.4f} / Bandwidth 90% {:.4f}".format(bw, bw_90))
+    Bandwidth: 473.7305 / Bandwidth 90% 3186.9141
+
+    """    
+    # Compute spectral quantiles
+    if bw_method=="quantile":
+        q = spectral_quantile(s, fs, [0.05, 0.25, 0.75, 0.95], nperseg, roi, as_pandas_series, **kwargs)
+    elif bw_method=="3dB":
+        pass
+
+    # Compute spectral bandwidth
+    if as_pandas_series: 
+        return pd.Series([np.abs(q.iloc[2]-q.iloc[1]), np.abs(q.iloc[3]-q.iloc[0])], index=["bandwidth", "bandwidth_90"])
+    else:                
+        return np.array([np.abs(q[2]-q[1]), np.abs(q[3]-q[0])])
+#%% ---------------
+# # Frequencia
+# # - peak frequency      ok
+# # - spectrum quantile   ok
+# # - Bandwidth           ok (3dB missing method)
+# # - max freq            ok
+# # - min freq            ok
+# # Tiempo
+# # - quantile                 ok
+# # - Center time              ok
+# # - Duration                 ok
+# # - Call rate / Pulse Rate     https://www.avisoft.com/tutorials/measuring-sound-parameters-from-the-spectrogram-automatically/
+# # Spectro-temporal            timeR seewave
+# # - pitch tracking
+
+
+# info, sox 
+# load
--- maad/features/temporal.py
+++ maad/features/temporal.py
@@ -13,9 +13,13 @@ Collection of functions to extract features from music
 # =============================================================================
 # Import external modules
 import numpy as np
+import pandas as pd
+from numpy.lib.function_base import _quantile_is_valid
+from scipy import interpolate
 
 # Import internal modules
 from maad.util import moments
+from maad.sound import envelope, trim
 
 #%%
 # =============================================================================
@@ -97,6 +101,296 @@ def zero_crossing_rate(s, fs):
     return zcr
 
 
+#%%
+def temporal_quantile(s, fs, q=[0.05, 0.25, 0.5, 0.75, 0.95], roi=None, mode="fast", Nt=32, as_pandas_series=False):
+    """
+    Compute the q-th temporal quantile of the waveform. If a region of interest with time and spectral limits is provided,
+    the q-th temporal quantile is computed on the selection.
 
+    Parameters
+    ----------
+    s : 1D array
+        Input audio signal
+    fs : float
+        Sampling frequency of audio signal
+    q : array or float, optional
+        Quantile or sequence of quantiles to compute, which must be between 0 and 1
+        inclusive.
+        The defaul is [0.05, 0.25, 0.5, 0.75, 0.95].
+    roi : pandas.Series, optional
+        Region of interest where peak frequency will be computed. 
+        Series must have a valid input format with index: min_t, min_f, max_t, max_f.
+        The default is None.
+    mode : str, optional, default is `fast`
+        - `fast` : The sound is first divided into frames (2d) using the 
+            function wave2timeframes(s), then the max of each frame gives a 
+            good approximation of the envelope.
+        - `Hilbert` : estimation of the envelope from the Hilbert transform. 
+            The method is slow
+    Nt : integer, optional, default is `32`
+        Size of each frame. The largest, the highest is the approximation.
+    as_pandas_series: bool
+        Return data as a pandas.Series. This is usefull when computing multiple features
+        over a signal. Default is False.
+    Returns
+    -------
+    Pandas Series or Numpy array
+        Temporal quantiles of waveform. 
 
+    Examples
+    --------
+    >>> from maad import features, sound
+    >>> s, fs = sound.load('../../data/spinetail.wav')
+
+    Compute the q-th temporal quantile of the wave energy
+
+    >>> qt = features.temporal_quantile(s, fs, [0.05, 0.25, 0.5, 0.75, 0.95])
+    >>> print("5%: {:.2f} / 25%: {:.2f} / 50%: {:.2f} / 75%: {:.2f} / 95%: {:.2f}".format(qt[0], qt[1], qt[2], qt[3], qt[4]))
+    5%: 1.22 / 25%: 5.71 / 50%: 11.82 / 75%: 16.36 / 95%: 17.76
+
+    """
+    q = np.asanyarray(q)
+    if not _quantile_is_valid(q):
+        raise ValueError("Percentiles must be in the range [0, 100]")
+
+    if roi is None:
+        min_t = 0
+    else:
+        s = trim(s, fs, min_t = roi.min_t, max_t = roi.max_t)
+        min_t = roi.min_t
+
+    env = envelope(s, mode, Nt)
+    t = min_t+np.arange(0,len(env),1)*len(s)/fs/len(env)
+    energy = pd.Series(env**2, index=t)
+
+    # Compute temporal quantile
+    norm_cumsum = energy.cumsum()/energy.sum()
+    spec_quantile = list()
+    for quantile in q:
+        spec_quantile.append(energy.index[np.where(norm_cumsum>=quantile)[0][0]])
     
+    if as_pandas_series:  return pd.Series(spec_quantile, index=[qth for qth in q])
+    else:                 return np.array(spec_quantile)
+    
+#%%
+def temporal_duration(s, fs, roi=None, mode="fast", Nt=32, as_pandas_series=False):
+    """ 
+    Compute the temporal duration of the waveform. If a region of interest with time and spectral limits is provided,
+    the temporal duration is computed on the selection.
+
+    Parameters
+    ----------
+    s : 1D array
+        Input audio signal
+    fs : float
+        Sampling frequency of audio signal
+    roi : pandas.Series, optional
+        Region of interest where peak frequency will be computed. 
+        Series must have a valid input format with index: min_t, min_f, max_t, max_f.
+        The default is None.
+    mode : str, optional, default is `fast`
+        - `fast` : The sound is first divided into frames (2d) using the 
+            function wave2timeframes(s), then the max of each frame gives a 
+            good approximation of the envelope.
+        - `Hilbert` : estimation of the envelope from the Hilbert transform. 
+            The method is slow
+    Nt : integer, optional, default is `32`
+        Size of each frame. The largest, the highest is the approximation.
+    as_pandas_series: bool
+        Return data as a pandas.Series. This is usefull when computing multiple features
+        over a signal. Default is False.
+    Returns
+    -------
+    duration : float 
+        Duration 50% of the audio
+    duration_90 : float 
+        Duration 90%  of the audio
+    
+    Examples
+    --------
+    >>> from maad import features, sound
+    >>> s, fs = sound.load('../../data/spinetail.wav')
+    
+    Compute the temporal duration of the time energy
+
+    >>> duration, duration_90 = features.temporal_duration(s, fs)
+    >>> print("Duration: {:.4f} / Duration 90% {:.4f}".format(duration, duration_90))
+    Duration: 10.6493 / Duration 90% 16.5451
+
+    """
+    # Compute temporal quantile
+    q = temporal_quantile(s, fs, [0.05, 0.25, 0.75, 0.95], roi, mode, Nt, as_pandas_series)
+    
+    # Compute temporal duration
+    if as_pandas_series: 
+        return pd.Series([np.abs(q.iloc[2]-q.iloc[1]), np.abs(q.iloc[3]-q.iloc[0])], index=["duration", "duration_90"])
+    else:                
+        return np.array([np.abs(q[2]-q[1]), np.abs(q[3]-q[0])])
+
+#%%
+def min_time(s, fs, method='best', roi=None, mode='fast', Nt=32, as_pandas_series=False):
+    """
+    Compute the minimum time for audio signal. The minimum time is the time of
+    minimum energy. If a region of interest with time and spectral limits is provided,
+    the minimum time is computed on the selection.
+    
+    Parameters
+    ----------
+    s : 1D array
+        Input audio signal
+    fs : float
+        Sampling frequency of audio signal
+    method : {'fast', 'best'}, optional
+        Method used to compute the minimum time. 
+        The default is 'best'.
+    roi : pandas.Series, optional
+        Region of interest where minimum time will be computed. 
+        Series must have a valid input format with index: min_t, min_f, max_t, max_f.
+        The default is None.
+    mode : str, optional, default is `fast`
+        - `fast` : The sound is first divided into frames (2d) using the 
+            function wave2timeframes(s), then the max of each frame gives a 
+            good approximation of the envelope.
+        - `Hilbert` : estimation of the envelope from the Hilbert transform. 
+            The method is slow
+    Nt : integer, optional, default is `32`
+        Size of each frame. The largest, the highest is the approximation.
+    as_pandas_series: bool
+        Return data as a pandas.Series. This is usefull when computing multiple features
+        over a signal. Default is False.
+    
+    Returns
+    -------
+    min_time : float
+        Minimum time of audio segment.
+    amp_min_time : float
+        Amplitud of the minimum time of audio segment.
+
+    Examples
+    --------
+    >>> from maad import features, sound
+    >>> s, fs = sound.load('../../data/spinetail.wav')
+    
+    Compute peak time with the fast method 
+    
+    >>> min_time, amp_min_time = features.min_time(s, fs)
+    >>> print('Minimum Time: {:.5f} / Minimum Time Amplitud: {:.5E}'.format(min_time, amp_min_time))
+    Minimum Time: 3.78561 / Minimum Time Amplitud: 1.80306E-06
+
+    """
+    if roi is None:
+        min_t = 0
+    else:
+        s = trim(s, fs, min_t = roi.min_t, max_t = roi.max_t)
+        min_t = roi.min_t
+    
+    # compute envelope, time, and energy
+    env = envelope(s, mode, Nt)
+    t = min_t+np.arange(0,len(env),1)*len(s)/fs/len(env)
+    energy = pd.Series(env**2, index=t)
+        
+    # simplest form to get peak frequency, but less precise
+    if method=='fast':
+        min_time = energy.idxmin() 
+        amp_min_time = energy[min_time]
+    
+    elif method=='best':        
+        # use interpolation get more precise peak frequency estimation
+        idxmin = energy.index.get_loc(energy.idxmin())
+        x = energy.iloc[[idxmin-1, idxmin, idxmin+1]].index.values
+        y = energy.iloc[[idxmin-1, idxmin, idxmin+1]].values
+        f = interpolate.interp1d(x,y, kind='quadratic')
+        xnew = np.arange(x[0], x[2], 1)
+        ynew = f(xnew) 
+        min_time = xnew[ynew.argmax()]
+        amp_min_time = ynew[ynew.argmax()]
+
+    else:
+        raise Exception("Invalid method. Method should be 'fast' or 'best' ")
+    
+    if as_pandas_series:    return pd.Series([amp_min_time], index=[min_time])
+    else:                   return np.array([min_time, amp_min_time])
+
+#%%
+def peak_time(s, fs, method='best', roi=None, mode='fast', Nt=32, as_pandas_series=False):
+    """
+    Compute the peak time for audio signal. The peak time is the time of
+    maximum energy. If a region of interest with time and spectral limits is provided,
+    the peak time is computed on the selection.
+    
+    Parameters
+    ----------
+    s : 1D array
+        Input audio signal
+    fs : float
+        Sampling frequency of audio signal
+    method : {'fast', 'best'}, optional
+        Method used to compute the peak time. 
+        The default is 'best'.
+    roi : pandas.Series, optional
+        Region of interest where peak time will be computed. 
+        Series must have a valid input format with index: min_t, min_f, max_t, max_f.
+        The default is None.
+    mode : str, optional, default is `fast`
+        - `fast` : The sound is first divided into frames (2d) using the 
+            function wave2timeframes(s), then the max of each frame gives a 
+            good approximation of the envelope.
+        - `Hilbert` : estimation of the envelope from the Hilbert transform. 
+            The method is slow
+    Nt : integer, optional, default is `32`
+        Size of each frame. The largest, the highest is the approximation.
+    as_pandas_series: bool
+        Return data as a pandas.Series. This is usefull when computing multiple features
+        over a signal. Default is False.
+    
+    Returns
+    -------
+    peak_time : float
+        Peak time of audio segment.
+    amp_peak_time : float
+        Amplitud of the peak time of audio segment.
+
+    Examples
+    --------
+    >>> from maad import features, sound
+    >>> s, fs = sound.load('../../data/spinetail.wav')
+    
+    Compute peak time with the fast method 
+    
+    >>> peak_time, peak_time_amp = features.peak_time(s, fs)
+    >>> print('Peak Time: {:.5f} / Peak Time Amplitud: {:.5f}'.format(peak_time, peak_time_amp))
+    Peak Time: 1.34967 / Peak Time Amplitud: 0.17552
+
+    """
+    if roi is None:
+        min_t = 0
+    else:
+        s = trim(s, fs, min_t = roi.min_t, max_t = roi.max_t)
+        min_t = roi.min_t
+
+    # compute envelope, time, and energy
+    env = envelope(s, mode, Nt)
+    t = min_t+np.arange(0,len(env),1)*len(s)/fs/len(env)
+    energy = pd.Series(env**2, index=t)
+        
+    # simplest form to get peak frequency, but less precise
+    if method=='fast':
+        peak_time = energy.idxmax() 
+        amp_peak_time = energy[peak_time]
+    
+    elif method=='best':        
+        # use interpolation get more precise peak frequency estimation
+        idxmax = energy.index.get_loc(energy.idxmax())
+        x = energy.iloc[[idxmax-1, idxmax, idxmax+1]].index.values
+        y = energy.iloc[[idxmax-1, idxmax, idxmax+1]].values
+        f = interpolate.interp1d(x,y, kind='quadratic')
+        xnew = np.arange(x[0], x[2], 1)
+        ynew = f(xnew) 
+        peak_time = xnew[ynew.argmax()]
+        amp_peak_time = ynew[ynew.argmax()]
+
+    else:
+        raise Exception("Invalid method. Method should be 'fast' or 'best' ")
+    
+    if as_pandas_series:    return pd.Series([amp_peak_time], index=[peak_time])
+    else:                   return np.array([peak_time, amp_peak_time])
