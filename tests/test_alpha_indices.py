274f23cc40feba378a9998698d5e71f21846bb1c
saguileran <saguileran@unal.edu.co>
2023-02-07 01:34:07 -0500

    testing withput install maad

--- tests/test_alpha_indices.py
+++ tests/test_alpha_indices.py
@@ -9,13 +9,15 @@ import os
 import pytest
 import maad
 import numpy as np
-
+import pandas as pd
+from pandas.testing import assert_frame_equal
+from pathlib import Path
 
 #%% Temporal indices
 
 def test_temporal_median():
     # Compute temporal median
-    s, fs = maad.sound.load('./data/spinetail.wav')
+    s, fs = maad.sound.load(Path('./data/spinetail.wav'))
     temporal_median_index = maad.features.temporal_median(s)
 
     # Load expected values
@@ -25,7 +27,7 @@ def test_temporal_median():
 
 def test_temporal_entropy():
     # Compute temporal median
-    s, fs = maad.sound.load('./data/spinetail.wav')
+    s, fs = maad.sound.load(Path('./data/spinetail.wav'))
     temporal_entropy_index = maad.features.temporal_entropy(s)
 
     # Load expected values
@@ -35,7 +37,7 @@ def test_temporal_entropy():
 
 def test_temporal_activity():
     # Compute temporal median
-    s, fs = maad.sound.load('./data/spinetail.wav')
+    s, fs = maad.sound.load(Path('./data/spinetail.wav'))
     temporal_activity_index = maad.features.temporal_activity(s, dB_threshold=6)
 
     # Load expected values
@@ -48,7 +50,7 @@ def test_temporal_activity():
 
 def test_spectral_entropy():
     # Compute spectral entropy
-    s, fs = maad.sound.load('./data/cold_forest_daylight.wav')
+    s, fs = maad.sound.load(Path('./data/cold_forest_daylight.wav'))
     Sxx_power, tn, fn, _ = maad.sound.spectrogram (s, fs)
     spectral_entropy_indices = maad.features.spectral_entropy(
         Sxx_power, fn, flim=(2000,10000))
@@ -74,4 +76,48 @@ def test_spectral_entropy():
 
 #%% Sepectral-temporal features
 
-#def test_spectra_temporal_features
+def test_spectra_features():
+    s, fs = maad.sound.load(Path('./data/spinetail.wav'))
+
+    spectral_features = maad.features.all_spectral_features(s, fs)
+
+    expected_values = pd.DataFrame({"sm":2.2763302268911505e-06,
+                                    "sv":8.11804174130019e-11,
+                                    "ss":5.8446635093417045,
+                                    "sk":40.488906373539436,
+                                    "Freq 5%":6029.296875,
+                                    "Freq 25%":6416.89453125,
+                                    "Freq 50%":6632.2265625,
+                                    "Freq 75%":6890.625,
+                                    "Freq 95%":9216.2109375,
+                                    "peak_freq":6632.2265625,
+                                    "Time 5%":1.2190476190476192,
+                                    "Time 25%":5.712108843537415,
+                                    "Time 50%":11.818956916099774,
+                                    "Time 75%":16.555827664399093,
+                                    "Time 95%":17.751655328798186,
+                                    "bandwidth_50":473.73046875,
+                                    "bandwidth_90":3186.9140625,
+                                    "bandwidth_3dB":320.8791300822477}, index=[0])
+
+    assert_frame_equal(spectral_features, expected_values)
+
+def test_temporal_features():
+    s, fs = maad.sound.load(Path('./data/spinetail.wav'))
+
+    temporal_features = maad.features.all_temporal_features(s, fs)
+
+    expected_values = pd.DataFrame({"sm":-2.043264e-19,
+                                    "sv":0.0011670735714956646,
+                                    "ss":-0.006548,
+                                    "sk":24.711611,
+                                    "Time 5%":1.215429,
+                                    "Time 25%":5.707076,
+                                    "Time 50%":11.816876,
+                                    "Time 75%":16.356414,
+                                    "Time 95%":17.760507,
+                                    "zcr":10500.397192,
+                                    "duration_50":10.649338,
+                                    "duration_90":16.545078}, index=[0])
+
+    assert_frame_equal(temporal_features, expected_values)
