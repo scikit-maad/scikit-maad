31d9ab539de377ad8cc0ed675bae544ddccb4c56
Bengt Lüers <bengt.lueers@gmail.com>
2023-02-11 02:59:57 +0100

    Name all parameters

--- example_gallery/basic/plot_audio_representation.py
+++ example_gallery/basic/plot_audio_representation.py
@@ -22,7 +22,10 @@ def plot_audio_representation(filename):
     # Load an audio file
     signal, sample_rate = sound.load(filename)
     # Plot and show the waveform
-    util.plot_wave(signal, sample_rate)
+    util.plot_wave(
+        s=signal,
+        fs=sample_rate,
+    )
     plt.show()
 
     # It can be noticed that in this audio there are four consecutive songs
@@ -31,52 +34,70 @@ def plot_audio_representation(filename):
     # seconds.
 
     # Trim the signal to zoom in on the details of the song.
-    signal_trimmed = sound.trim(signal, sample_rate, 5, 8)
+    signal_trimmed = sound.trim(
+        s=signal,
+        fs=sample_rate,
+        min_t=5,
+        max_t=8,
+    )
 
     # Compute the envelope of the trimmed signal
-    envelope = sound.envelope(signal_trimmed, mode='fast', Nt=128)
+    envelope_transformation_mode = 'fast'
+    frame_size = 128
+    envelope = sound.envelope(
+        s=signal_trimmed,
+        mode=envelope_transformation_mode,
+        Nt=frame_size,
+    )
 
-    # Compute the Fourier and short - time Fourier transforms
+    # Compute the Fourier and short-time Fourier transforms
+    sequence_size_in_samples = 1024
+    spectrum_power_density_estimation_method = 'welch'
     power_spectral_density_estimate, sample_frequency_indices = \
         sound.spectrum(
-            signal,
-            sample_rate,
-            nperseg=1024,
-            method='welch',
+            s=signal,
+            fs=sample_rate,
+            nperseg=sequence_size_in_samples,
+            method=spectrum_power_density_estimation_method,
         )
+
+    # Compute the spectrogram
+    spectrogram_window_type = 'hann'
+    spectrogram_fft_window_size_in_samples = 1024
+    spectrogram_segment_overlap_in_samples = 512
     Sxx_power_or_amplitude, time_vector_of_the_horizontal_x_axis,\
         frequency_vector_of_the_vertical_y_axis, box_extend = \
         sound.spectrogram(
-            signal_trimmed,
-            sample_rate,
-            window='hann',
-            nperseg=1024,
-            noverlap=512,
+            x=signal_trimmed,
+            fs=sample_rate,
+            window=spectrogram_window_type,
+            nperseg=spectrogram_fft_window_size_in_samples,
+            noverlap=spectrogram_segment_overlap_in_samples,
         )
 
     # Visualize the signal characteristics in the temporal and spectral domains
     figure, axes = plt.subplots(
-        4,
-        1,
+        nrows=4,
+        ncols=1,
         figsize=(8, 10),
     )
     util.plot_wave(
-        signal_trimmed,
-        sample_rate,
+        s=signal_trimmed,
+        fs=sample_rate,
         ax=axes[0],
     )
     util.plot_wave(
-        envelope,
-        sample_rate,
+        s=envelope,
+        fs=sample_rate,
         ax=axes[1],
     )
     util.plot_spectrum(
-        power_spectral_density_estimate,
-        sample_frequency_indices,
+        pxx=power_spectral_density_estimate,
+        f_idx=sample_frequency_indices,
         ax=axes[2],
     )
     util.plot_spectrogram(
-        Sxx_power_or_amplitude,
+        Sxx=Sxx_power_or_amplitude,
         extent=box_extend,
         ax=axes[3],
         colorbar=False,
