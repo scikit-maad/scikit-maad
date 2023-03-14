bf2eaf2ca8156f25a220798e15791b0bb336461a
Juan Sebastian Ulloa <jseb.ulloa@gmail.com>
2023-03-13 22:31:09 -0500

    TEST: Added more test coverage for shape features

--- tests/test_shape_features.py
+++ tests/test_shape_features.py
@@ -10,10 +10,38 @@ import pandas as pd
 from maad import sound, features, util
 
 
-def test_shape_features():
-    DATA = pd.read_csv(os.path.join('./tests/data/','shape_spinetail_res_low.csv'))
-    s, fs = sound.load(os.path.join('./data/','spinetail.wav'))
+def test_shape_features_low():
+    EXPECTED_SHAPE = np.load(os.path.join('tests','data','shape_spinetail_res_low.npy'))
+    EXPECTED_PARAMS = np.load(os.path.join('tests','data','params_res_low.npy'))
+    s, fs = sound.load(os.path.join('data','spinetail.wav'))
     Sxx, tn, fn, ext = sound.spectrogram(s, fs, db_range=100)
     Sxx_db = util.power2dB(Sxx, db_range=100)
     shape, params = features.shape_features(Sxx_db, resolution='low')
-    assert np.allclose(shape,DATA)
+    assert np.allclose(shape.values, EXPECTED_SHAPE), \
+        'shape not equal'
+    assert np.allclose(params.values, EXPECTED_PARAMS), \
+        'params not equal'
+
+def test_shape_features_med():
+    EXPECTED_SHAPE = np.load(os.path.join('tests','data','shape_spinetail_res_med.npy'))
+    EXPECTED_PARAMS = np.load(os.path.join('tests','data','params_res_med.npy'))
+    s, fs = sound.load(os.path.join('./data/','spinetail.wav'))
+    Sxx, tn, fn, ext = sound.spectrogram(s, fs, db_range=100)
+    Sxx_db = util.power2dB(Sxx, db_range=100)
+    shape, params = features.shape_features(Sxx_db, resolution='med')
+    assert np.allclose(shape.values, EXPECTED_SHAPE), \
+        'shape not equal'
+    assert np.allclose(params.values, EXPECTED_PARAMS), \
+        'params not equal'
+    
+def test_shape_features_high():
+    EXPECTED_SHAPE = np.load(os.path.join('tests','data','shape_spinetail_res_high.npy'))
+    EXPECTED_PARAMS = np.load(os.path.join('tests','data','params_res_high.npy'))
+    s, fs = sound.load(os.path.join('./data/','spinetail.wav'))
+    Sxx, tn, fn, ext = sound.spectrogram(s, fs, db_range=100)
+    Sxx_db = util.power2dB(Sxx, db_range=100)
+    shape, params = features.shape_features(Sxx_db, resolution='high')
+    assert np.allclose(shape.values, EXPECTED_SHAPE), \
+        'shape not equal'
+    assert np.allclose(params.values, EXPECTED_PARAMS), \
+        'params not equal'
