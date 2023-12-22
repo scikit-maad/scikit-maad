8e8d08195e09ee17536f7c47b55feff485e993ac
Juan Sebastian Ulloa <jseb.ulloa@gmail.com>
2023-12-22 14:28:17 -0500

    ENH: Added functionality to parallelize graphical_soundscapes
    ENH: Added functionality to parallelize graphical_soundscapes

--- maad/features/composite_soundscape_descriptors.py
+++ maad/features/composite_soundscape_descriptors.py
@@ -2,6 +2,7 @@
 Utility functions for computing composite soundscape descriptors using data from multiple files at the same sampling site.
 """
 import os
+from concurrent.futures import ProcessPoolExecutor
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -29,9 +30,87 @@ def _input_validation(data_input):
         raise ValueError("Input 'data' must be either a Pandas DataFrame or a file path string")
     return df
 
+def _validate_n_jobs(n_jobs):
+    """ Validate number of jobs """
+    if not isinstance(n_jobs, int):
+        raise ValueError("n_jobs must be an integer.")
+    
+    if n_jobs <= 0 or n_jobs == -1:
+        # Use all available CPUs
+        n_jobs = os.cpu_count()
+        print(f"Using all available CPUs: {n_jobs}")
+    else:
+        # Validate that the number is not larger than available CPUs
+        available_cpus = os.cpu_count()
+        if n_jobs > available_cpus:
+            # Set n_jobs to the maximum number of available CPUs
+            n_jobs = available_cpus
+            print(f"Adjusted n_jobs to maximum available CPUs: {n_jobs}")
+        else:
+            print(f"Using specified number of CPUs: {n_jobs}")
+
+    return n_jobs
+
+#%%
+def _spectral_peak_density(
+        path_audio, target_fs, nperseg, noverlap, db_range, min_distance, threshold_abs):
+    """
+    Computes the spectral peak density for an audio file, representing the number of peaks per time step within each frequency bin.
+
+    Parameters
+    ----------
+    path_audio : pandas DataFrame
+        A Pandas DataFrame containing information about the audio files.
+    target_fs : int
+        The target sample rate to resample the audio signal if needed.
+    nperseg : int
+        Window length of each segment to compute the spectrogram.
+    noverlap : int
+        Number of samples to overlap between segments to compute the spectrogram.
+    db_range : float
+        Dynamic range of the computed spectrogram.
+    min_distance : int
+        Minimum number of indices separating peaks.
+    threshold_abs : float
+        Minimum amplitude threshold for peak detection in decibels.
+
+    Returns
+    -------
+    peak_density : pandas Dataframe
+        The peak density representation of the audio per frequency bin.
+
+    """
+
+    filename = os.path.basename(path_audio)
+    print(f'Processing file {filename}', end='\r')
+
+    # Load data
+    s, fs = sound.load(path_audio)
+    s = sound.resample(s, fs, target_fs, res_type="scipy_poly")
+    Sxx, tn, fn, ext = sound.spectrogram(s, target_fs, nperseg=nperseg, noverlap=noverlap)
+    Sxx_db = util.power2dB(Sxx, db_range=db_range)
+
+    # Compute local max
+    _, peak_freq = rois.spectrogram_local_max(
+        Sxx_db, tn, fn, ext,
+        min_distance, 
+        threshold_abs)
+
+    # Compute peak density (number of peaks / time steps)
+    freq_idx, count_freq = np.unique(peak_freq, return_counts=True)
+    count_peak = np.zeros(fn.shape)
+    bool_index = np.isin(fn, freq_idx)
+    indices = np.where(bool_index)[0]
+    count_peak[indices] = count_freq / len(tn)
+    peak_density = pd.Series(index=fn, data=count_peak)
+
+    # Normalize per time
+    peak_density.name = filename
+    return peak_density.to_frame().T
+
 #%%
 def graphical_soundscape(
-    data, threshold_abs, path_audio='path_audio', time='time', target_fs=48000, nperseg=256, noverlap=128, db_range=80, min_distance=1):
+    data, threshold_abs, path_audio='path_audio', time='time', target_fs=48000, nperseg=256, noverlap=128, db_range=80, min_distance=1, n_jobs=1):
     """
     Computes a graphical soundscape from a given DataFrame of audio files.
 
@@ -62,6 +141,8 @@ def graphical_soundscape(
         Dynamic range of the computed spectrogram.
     min_distance : int
         Minimum number of indices separating peaks.
+    n_jobs : int
+        Number of processes to use for parallel computing. Default is 1.
 
     Returns
     -------
@@ -76,41 +157,29 @@ def graphical_soundscape(
     >>> gs = graphical_soundscape(data=df, threshold_abs=-80)
     >>> plot_graph(gs)
     """
-
     df = _input_validation(data)
     df.sort_values(by=path_audio, inplace=True)
     total_files = len(df)
     print(f'{total_files} files found to process...')
-    res = pd.DataFrame()
-    for idx, df_aux in df.reset_index().iterrows():
-        filename = os.path.basename(df_aux[path_audio])
-        print(f'Processing file {idx+1} of {total_files}: {filename}', end='\r')
+    flist = df[path_audio].to_list()
+
+    if n_jobs==1:
+        # Use sequential processing
+        results = []
+        for path_audio in flist:
+            result = _spectral_peak_density(path_audio, target_fs, nperseg, noverlap, db_range, min_distance, threshold_abs)
+            results.append(result)
+    else:
+        # Use parallel processing
+        _validate_n_jobs(n_jobs)
+        with ProcessPoolExecutor() as executor:
+            futures = [executor.submit(_spectral_peak_density, path_audio, target_fs, nperseg, noverlap, db_range, min_distance, threshold_abs)
+                       for path_audio in flist]
+
+        # Wait for all tasks to complete
+        results = [future.result() for future in futures]
         
-        # Load data
-        s, fs = sound.load(df_aux[path_audio])
-        s = sound.resample(s, fs, target_fs, res_type="scipy_poly")
-        Sxx, tn, fn, ext = sound.spectrogram(s, target_fs, nperseg=nperseg, noverlap=noverlap)
-        Sxx_db = util.power2dB(Sxx, db_range=db_range)
-
-        # Compute local max
-        peak_time, peak_freq = rois.spectrogram_local_max(
-            Sxx_db, tn, fn, ext,
-            min_distance, 
-            threshold_abs)
-        
-        # Count number of peaks at each frequency bin
-        freq_idx, count_freq = np.unique(peak_freq, return_counts=True)
-        count_peak = np.zeros(fn.shape)
-        bool_index = np.isin(fn, freq_idx)
-        indices = np.where(bool_index)[0]
-        count_peak[indices] = count_freq / len(tn)
-        peak_density = pd.Series(index=fn, data=count_peak)
-
-        # Normalize per time
-        #peak_density = (peak_density > 0).astype(int)
-        peak_density.name = os.path.basename(df_aux[path_audio])
-        res = pd.concat([res, peak_density.to_frame().T])
-
+    res = pd.concat(results)
     res["time"] = df[time].str[0:2].astype(int).to_numpy()
     print('\nComputation completed!')
     return res.groupby("time").mean()
