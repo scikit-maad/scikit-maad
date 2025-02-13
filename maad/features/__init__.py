f2ce9bbe8ee9e5ecf080a5bbc7ff91c8e71fb1d2
shaupert <sylvain.haupert@mnhn.fr>
2025-02-13 22:11:14 +0100

    [DEV] add new alpha indices nROI and aROI + new tests

--- maad/features/__init__.py
+++ maad/features/__init__.py
@@ -48,6 +48,7 @@ Alpha acoustic indices
     more_entropy
     acoustic_gradient_index
     frequency_raoq
+    region_of_interest_index_deprecated
     region_of_interest_index
     all_temporal_alpha_indices
     all_spectral_alpha_indices
@@ -85,24 +86,24 @@ Composite acoustic features
 """
 
 from .shape import (filter_multires,
-                          filter_bank_2d_nodc,
-                          opt_shape_presets,
-                          shape_features,
-                          shape_features_raw,
-                          centroid_features,
-                          all_shape_features)
+                    filter_bank_2d_nodc,
+                    opt_shape_presets,
+                    shape_features,
+                    shape_features_raw,
+                    centroid_features,
+                    all_shape_features)
 
 from .spectral import (spectral_moments,
-                       peak_frequency,
-                       spectral_quantile,
-                       spectral_bandwidth,
-                       all_spectral_features)
+                    peak_frequency,
+                    spectral_quantile,
+                    spectral_bandwidth,
+                    all_spectral_features)
 
 from .temporal import (temporal_moments,
-                       zero_crossing_rate,
-                       temporal_duration,
-                       temporal_quantile,
-                       all_temporal_features)
+                    zero_crossing_rate,
+                    temporal_duration,
+                    temporal_quantile,
+                    all_temporal_features)
 
 from .alpha_indices import (temporal_median,
                             temporal_entropy,
@@ -128,69 +129,71 @@ from .alpha_indices import (temporal_median,
                             more_entropy,
                             acoustic_gradient_index,
                             frequency_raoq,
+                            region_of_interest_index_deprecated,
                             region_of_interest_index,
                             all_temporal_alpha_indices,
                             all_spectral_alpha_indices)
 
 from .composite_soundscape_descriptors import (graphical_soundscape,
-                                               plot_graph)
+                                            plot_graph)
 
 __all__ = [
-           # shape
-           'filter_multires', 
-           'filter_bank_2d_nodc',
-           'opt_shape_presets',
-           'shape_features',
-           'shape_features_raw',
-           'centroid_features',
-           'all_shape_features',
-           
-           # spectral
-           'spectral_moments',
-           'peak_frequency',
-           'spectral_quantile',
-           'temporal_quantile',
-           'spectral_bandwidth',
-           'all_spectral_features',
-           
-           # temporal
-           'temporal_moments',
-           'zero_crossing_rate',
-           'temporal_duration',
-           'all_temporal_features',
-           
-           # alpha_indices
-           'temporal_moments',
-           'temporal_median',
-           'temporal_entropy',
-           'acoustic_richness_index',
-           "temporal_activity",
-           "temporal_events",
-           "acoustic_complexity_index",
-           "frequency_entropy",
-           "number_of_peaks",
-           "spectral_entropy",
-           "spectral_moments",
-           "spectral_activity",
-           "spectral_events",
-           'spectral_cover',
-           "soundscape_index",
-           'bioacoustics_index',
-           "acoustic_diversity_index",
-           "acoustic_eveness_index",
-           "roughness",
-           'temporal_leq',
-           'spectral_leq',
-           "surface_roughness",
-           "tfsd",
-           "more_entropy",
-           "acoustic_gradient_index",
-           "frequency_raoq",
-           "region_of_interest_index",
-           'all_temporal_alpha_indices',
-           'all_spectral_alpha_indices',
-           
-           # composite features
-           'graphical_soundscape',
-           'plot_graph',
-           ]
+        # shape
+        'filter_multires', 
+        'filter_bank_2d_nodc',
+        'opt_shape_presets',
+        'shape_features',
+        'shape_features_raw',
+        'centroid_features',
+        'all_shape_features',
+        
+        # spectral
+        'spectral_moments',
+        'peak_frequency',
+        'spectral_quantile',
+        'temporal_quantile',
+        'spectral_bandwidth',
+        'all_spectral_features',
+        
+        # temporal
+        'temporal_moments',
+        'zero_crossing_rate',
+        'temporal_duration',
+        'all_temporal_features',
+        
+        # alpha_indices
+        'temporal_moments',
+        'temporal_median',
+        'temporal_entropy',
+        'acoustic_richness_index',
+        "temporal_activity",
+        "temporal_events",
+        "acoustic_complexity_index",
+        "frequency_entropy",
+        "number_of_peaks",
+        "spectral_entropy",
+        "spectral_moments",
+        "spectral_activity",
+        "spectral_events",
+        'spectral_cover',
+        "soundscape_index",
+        'bioacoustics_index',
+        "acoustic_diversity_index",
+        "acoustic_eveness_index",
+        "roughness",
+        'temporal_leq',
+        'spectral_leq',
+        "surface_roughness",
+        "tfsd",
+        "more_entropy",
+        "acoustic_gradient_index",
+        "frequency_raoq",
+        "region_of_interest_index_deprecated"
+        "region_of_interest_index",
+        'all_temporal_alpha_indices',
+        'all_spectral_alpha_indices',
+        
+        # composite features
+        'graphical_soundscape',
+        'plot_graph',
+        ]
